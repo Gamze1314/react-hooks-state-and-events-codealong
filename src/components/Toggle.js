5edaeae5d7f8a4fc8d5f6cd17f4271433108b9a4
gamze <gamzekandara@gmail.com>
2024-01-27 23:54:21 -0500

    Adds Toggle.js

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
+// useState() returns an array 
 
 function Toggle() {
-  return <button>OFF</button>;
+  // isOn variable => what text to display in the button?
+  const [ isOn, setIsOn ] = useState(false)
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn)
+  }
+
+  const color = isOn ? "red" : "white"
+  // conditional rendering based on state variable
+  return <button style={{ background: color }} onClick={handleClick}>{isOn ? "ON" : "OFF"}</button>;
 }
 
 export default Toggle;
